52f5b556b33b7f0fcdd3710a5ce5a1e487c5c264
smirta <simon.nussbaum@adfinis-sygroup.ch>
2019-12-03 10:47:36 +0100

    corrections

--- main.tf
+++ main.tf
@@ -1,36 +1,6 @@
-terraform {
-  backend "remote" {
-    organization = "technophilch"
-
-    workspaces {
-      name = "simuawsplayground"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  version = "~> 2.0"
-  region  = "us-west-2"
-}
-
-# Get the AWS Ubuntu image
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-  owners = ["099720109477"] # Canonical
-}
-
 # Create a VPC
 resource "aws_vpc" "main-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr
 }
 
 # Create subnet
@@ -58,10 +28,4 @@ resource "aws_route_table" "main-route" {
 resource "aws_route_table_association" "main-subnet-assoc" {
   subnet_id      = aws_subnet.main-subnet.id
   route_table_id = aws_route_table.main-route.id
-}
-
-# Public key for instance access
-resource "aws_key_pair" "admin" {
-  key_name = "admin" 
-  public_key = file(".ssh/id_rsa.pub")
 }
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "showCIDR" {
   value = aws_subnet.main-subnet.cidr_block
 }
 
+output "main-subnet-id" {
+  value = aws_vpc.main-subnet.id
+}
+
 output "main-vpc-id" {
-  aws_vpc.main-vpc.id
+  value = aws_vpc.main-vpc.id
 }
